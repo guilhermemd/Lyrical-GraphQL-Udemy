5ef44fd9140358d0a624f99df62044270f709a5e
GuilhermeMD <guilherme.md@gmail.com>
2021-12-23 22:30:54 -0300

    feat: :sparkles: video 40 - first query on front-end

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-
+import gql from "graphiql-tag";
 class SongList extends Component {
   render() {
     return <div>SongList</div>;
   }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
 
 export default SongList;
