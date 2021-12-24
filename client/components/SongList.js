0ac1fde630345112c32af45810bb186e5c6e42a0
GuilhermeMD <guilherme.md@gmail.com>
2021-12-24 17:43:57 -0300

    feat: :sparkles: video 49 - link btn to / and songs/new

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map((song) => {
       return (
-        <li key={song.title} className="collection-item">
+        <li key={song.id} className="collection-item">
           {song.title}
         </li>
       );
@@ -17,9 +18,17 @@ class SongList extends Component {
       return <div>Loading...</div>
     }
     return (
+      <div>
         <ul className="collection">
           {this.renderSongs()}
         </ul>
+        <Link 
+          to="/songs/new"
+          className="btn-floating btn-large red right"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
       );
   }
 }
