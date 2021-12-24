61a83b41a706ccc3d8ad81fddf846fc203a13702
GuilhermeMD <guilherme.md@gmail.com>
2021-12-23 23:12:20 -0300

    feat: :sparkles: video 41 - loading to render with map

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from 'react-apollo'; 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li key={song.title}>
+          {song.title}
+        </li>
+      );
+    });
+  }
   render() {
-    console.log(this.props);
+    if(this.props.data.loading) {
+      return <div>Loading...</div>
+    }
     return (
         <div>
-          SongList
+          {this.renderSongs()}
         </div>
       );
   }
