026c5f460a1afd0433ed02714f0f5817b23b8c2c
GuilhermeMD <guilherme.md@gmail.com>
2021-12-24 23:24:00 -0300

    feat: :sparkles: video 62 - lyric creation form

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LyricCreate extends Component {
+  render() {
+    return (
+      <form>
+        <labe>Add a Lyric</labe>
+        <input />
+      </form>
+    )
+  }
+}
+
+export default LyricCreate;
